0df56fe75be31b2e810eb9bf4be51af304487339
exp <exp.table@protonmail.com>
2021-12-24 00:05:52 +0100

    tidy up canvasv2 + inputs verification

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Canvas from './components/Canvas.vue'
+import Canvas from './components/CanvasV2.vue'
 </script>
 
 <template>
